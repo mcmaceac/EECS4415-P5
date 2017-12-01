b3ef22e10c7589f409fec87dd76a119f07535979
mcmaceac <mcm.maceachern@gmail.com>
2017-11-30 22:55:26 -0500

    8 edges being removed forming a distinct east coast region

--- city-between.py
+++ city-between.py
@@ -6,6 +6,7 @@ import re
 G = nx.Graph()
 positions = {} # a position map of the nodes (cities)
 
+#create the graph from the text file
 inFile = sys.stdin
 for line in inFile:
 	elmts = [x.strip() for x in line.split(',')]
@@ -14,17 +15,37 @@ for line in inFile:
 
 	indentedLines = int(elmts[4])
 	G.add_node(nodeName, pos=(float(elmts[2]), float(elmts[3])))
+
+	#add edges between nodes(indented elements)
 	while indentedLines > 0:
 		nextLine = next(inFile)
 		indentedLines -= 1
 		connectElements = [x.strip() for x in nextLine.split(',')]
 		#print(connectElements)
 		otherNodeName = connectElements[0] + ',' + connectElements[1]
-		G.add_edge(nodeName, otherNodeName)
-	#if indentedLines > 0:
+		G.add_edge(nodeName, otherNodeName, weight=int(connectElements[2]))
 
-	#else:
+#print(nx.get_edge_attributes(G, "weight"))
+weights = nx.get_edge_attributes(G, 'weight')
 
+#print(weights)
+#for edge in G.edges(data=True):
+#	print(edge)
+
+#print(nx.edge_betweenness_centrality(G, weight='weight'))
+#for betweenness in sorted(nx.edge_betweenness_centrality(G, weight='weight').items(), key=lambda x: -x[1]):
+i = 0
+while(nx.is_connected(G)):
+	betweenness = sorted(nx.edge_betweenness_centrality(G, weight='weight').items(), key=lambda x: -x[1])
+	G.remove_edge(betweenness[0][0][0], betweenness[0][0][1])
+	i += 1
+
+#print(betweenness)
+print("No longer connected! Edges removed: " + str(i))
+
+
+
+#print(G.number_of_nodes())
 positions = nx.get_node_attributes(G, 'pos')
 # plot the city graph
 plot.figure(figsize=(8,8))
